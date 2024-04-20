ec8fa45204ebe995cbf620bd9f58ad219e2daf01
aidqen <idanmarkin8@gmail.com>
2024-04-21 00:58:08 +0300

    Start renderTodos function

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,31 @@
 'use strict'
 
+import { userService } from 'js/service/user.service.js'
+import { utilService } from './service/util.service.js'
+
 window.app = {
-    onToggleCheckbox
+  onToggleCheckbox,
+  onInit,
+}
+
+
+
+function onInit() {
+  renderTodos()
+}
+
+function renderTodos() {
+  const elTodos = document.querySelector('.todos')
+  const todos = userService.getTodos()
+
+  todos.map(todo => {
+        `<div class="todo-container todo-${} flex flex-row flex-center">
+        <div class="checkbox" onclick="app.onToggleCheckbox(this)">✔</div>
+        <p></p>
+        </div>`
+  })
 }
 
 function onToggleCheckbox(elBtn) {
-    elBtn.classList.toggle('active')
+  elBtn.classList.toggle('active')
 }
---
+++ js/service/util.service.js
@@ -0,0 +1,14 @@
+'use strict'
+
+export const utilService = {
+    getRandomId
+}
+
+function getRandomId(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomId = '';
+    for (let i = 0; i < length; i++) {
+        randomId += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return randomId;
+}
